0cc96f62219ca9e953f4e7790bf3807ddce57f03
Christian Hotz <hotz@hydra-newmedia.com>
2019-06-04 16:20:38 +0200

    allow to pass sentry client; instantiate own only if needed

--- schema.js
+++ schema.js
@@ -6,6 +6,33 @@ const joi = require('joi');
 const levels = Object.values(Severity).filter(level => typeof level === 'string')
   || ['fatal', 'error', 'warning', 'log', 'info', 'debug', 'critical'];
 
+const sentryClient = joi.object().keys({
+  configureScope: joi.func().minArity(1),
+  Scope: joi.func().required(),
+  Parsers: joi.object().keys({
+    parseError: joi.func().minArity(1).required(),
+  }).unknown().required(),
+  Handlers: joi.object().keys({
+    parseRequest: joi.func().minArity(2).required(),
+  }).unknown().required(),
+  withScope: joi.func().minArity(1).required(),
+  captureEvent: joi.func().minArity(1).required(),
+}).unknown();
+
+const sentryOptions = joi.object().keys({
+  dsn: joi.string().uri().allow(false).required(),
+  debug: joi.boolean(),
+  release: joi.string(),
+  environment: joi.string(),
+  sampleRate: joi.number().min(0).max(1),
+  maxBreadcrumbs: joi.number().integer(),
+  attachStacktrace: joi.any(),
+  sendDefaultPii: joi.boolean(),
+  serverName: joi.string(),
+  beforeSend: joi.func(),
+  beforeBreadcrumb: joi.func(),
+});
+
 module.exports = joi.object().keys({
   baseUri: joi.string().uri(),
   channels: joi.array().items(joi.string().only(levels)).single().default('error'),
@@ -18,17 +45,5 @@ module.exports = joi.object().keys({
     level: joi.string().only(levels),
     extra: joi.object(),
   }),
-  client: joi.object().keys({
-    dsn: joi.string().uri().allow(false).required(),
-    debug: joi.boolean(),
-    release: joi.string(),
-    environment: joi.string(),
-    sampleRate: joi.number().min(0).max(1),
-    maxBreadcrumbs: joi.number().integer(),
-    attachStacktrace: joi.any(),
-    sendDefaultPii: joi.boolean(),
-    serverName: joi.string(),
-    beforeSend: joi.func(),
-    beforeBreadcrumb: joi.func(),
-  }).required(),
+  client: joi.alternatives().try([sentryOptions, sentryClient]).required(),
 });
