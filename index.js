0caf3998255a126f29af7ac4e71ab8adca409ee5
Diego Acosta <diegonake@gmail.com>
2019-10-01 17:32:29 +0200

    ignore events without error object

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ exports.register = (server, options) => {
 
   // get request errors to capture them with sentry
   server.events.on({ name: 'request', channels: ['error', 'app'] }, (request, event, tags) => {
-    if (event.channel === 'app' && !tags.error) {
+    if (event.channel === 'app' && (!tags.error || !event.error)) {
       return;
     }
 
@@ -76,7 +76,7 @@ exports.register = (server, options) => {
   });
 
   server.events.on('log', (event, tags) =>  {
-    if (!tags.error) {
+    if (!tags.error || !event.error) {
       return;
     }
 
